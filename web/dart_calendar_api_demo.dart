2aac68e479e9314f34790cc446a2e68830bb6572
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-06-22 12:50:33 +0200

    Updated to latest SDK

--- web/dart_calendar_api_demo.dart
+++ web/dart_calendar_api_demo.dart
@@ -1,43 +1,39 @@
 import 'dart:html';
 import 'dart:convert' show JSON;
-import 'package:chrome/app.dart';
 import 'package:intl/intl.dart';
+import 'package:chrome_gen/chrome_app.dart' as chrome;
 
 // URL da richiedere
-String requestUrl =
-  
-'https://www.googleapis.com/calendar/v3/calendars/MIO_INDIRIZZO_EMAIL/events?timeMin='+getFormattedDate();
+String requestUrl = getRequestUrl();
 
 // Elementi top-level
 ButtonElement logoutBtn = querySelector('#logout');
 ButtonElement loginBtn = querySelector('#login');
 DivElement events = querySelector('#events');
-
+List<String> colors = ['rgb(247, 237, 190)','rgb(223, 243, 215)',
+                       'rgb(251, 253, 186)', 'rgb(253, 229, 229)',
+                       'rgba(226, 241, 243, 0.42)'];
+int cnt = 0;
 void main() {
-  
   String token;
 
   loginBtn.onClick.listen((data) {
     // Otteniamo un token per l'autenticazione
-    chromeIdentity.getAuthToken(interactive: true).then((_token) {
+    var tokenDetails = new chrome.TokenDetails(interactive: true);
+    chrome.identity.getAuthToken(tokenDetails).then((_token) {
       token = _token;
       // Prepariamo la richiesta
       var req = new HttpRequest();
       req.onLoadEnd.listen((e) {
         if(req.status==200) {
-          // Nascondiamo il bottone di login
           loginBtn.style.visibility = 'hidden';
           loginBtn.style.display = 'none';
-          
-          // Mostriamo il bottone di logout
           logoutBtn.style.visibility = 'visible';
           logoutBtn.style.display = 'block';
-          
-          // Passiamo il response alla funzione showData
           showData(req.responseText);
         }
       });
-      // Inoltriamo la richiesta
+
       req
       ..open('GET', requestUrl)
       ..setRequestHeader('Authorization', 'Bearer $token')
@@ -48,10 +44,11 @@ void main() {
   // Registriamo una funzione di callback all'evento onClick su `logoutBtn`
   logoutBtn.onClick.listen((e) {
     if (token != null) {
+      var invalidTokenDetails = new chrome.InvalidTokenDetails(token: token);
       var req = new HttpRequest();
       req.onLoadEnd.listen((e) {
         // Rimuoviamo l'auth token dalla cache
-        chromeIdentity.removeCachedAuthToken(token).then((_){
+        chrome.identity.removeCachedAuthToken(invalidTokenDetails).then((_){
           loginBtn.style.visibility = 'visible';
           loginBtn.style.display = 'block';
           logoutBtn.style.visibility = 'hidden';
@@ -62,10 +59,17 @@ void main() {
         });
       // Inoltriamo la richiesta di revoca del token
       req
-      ..open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=${token}')
+      ..open("GET", "https://accounts.google.com/o/oauth2/revoke?token=$token")
       ..send();
     }
   });
+}
+
+String getRequestUrl() {
+  String endpoint = "https://www.googleapis.com/calendar/v3/calendars";
+  String email = "INSERT_YOUR_EMAIL_HERE";
+  String req = "events?timeMin=${getFormattedDate()}&singleEvents=true&orderBy=startTime";
+  return endpoint + '/' + email + '/' + req;
 }
 
 void showData(String _data) {
@@ -89,32 +93,44 @@ DivElement createDiv(data) {
   var elem = new DivElement();
   var summary = new ParagraphElement();
   var htmlLink = new AnchorElement();
+  var imgLink = new ImageElement();
   var info = new ParagraphElement();
-  
+
   // Impostiamo lo stile degli elementi, interessante la sintassi a cascata ".."
   elem.style
     ..marginBottom = '10px'
     ..padding = '10px'
-    ..borderRadius = '2px'
-    ..border = '1px solid #ccc'
-    ..backgroundColor = 'white'
-    ..fontSize = 'x-large';
-  
+    ..color = '#525252'
+    ..borderRadius = '1px'
+    ..position = 'relative'
+    ..backgroundColor = colors[cnt%colors.length];
+
   htmlLink
-    ..text = "Vedi evento su Google Calendar"
     ..href = data["htmlLink"]
-    ..style.fontSize = 'x-small'
+    ..style.position = "absolute"
+    ..style.right = "0"
+    ..style.top = "0"
+    ..style.marginTop = "5px"
+    ..style.marginRight = "5px"
     ..target = '_blank';
-  
+
+  imgLink.src = 'globe.png';
+
+  htmlLink.children.add(imgLink);
+
+  var creator = data["creator"]["displayName"] != null ?
+      data["creator"]["displayName"] : data["creator"]["email"];
+
   info
-  ..innerHtml = "${data["start"]["dateTime"]}<br/><em>Creato da ${data["creator"]["displayName"]}</em>"
-  ..style.fontSize = 'x-small';
-  
+  ..innerHtml = "${data["start"]["dateTime"]}<br/><em>Creato da $creator</em>";
+
   summary
     ..text = data["summary"]
+    ..style.fontSize = '18px'
     ..style.marginBottom = '5px'
     ..style.marginTop = '5px';
-   
-  elem.children.addAll([info,summary,htmlLink]);
+
+  elem.children.addAll([summary,info,htmlLink]);
+  cnt++;
   return elem;
-} 
+}
